bc0b3b0c482e5dd2bc67f044f38a9397a3e056bf
chenqilin70 <kylinchen85@foxmail.com>
2020-08-19 17:49:00 +0800

    测试git

--- alpha_lin/test.py
+++ alpha_lin/test.py
@@ -1,8 +1,21 @@
 
-from  alpha_lin.service.AccountBalanceService import AccountBalanceService
+from alpha_lin.service.AccountBalanceService import AccountBalanceService, MarketClient, CandlestickInterval
+
+
+def callback(candlestick_event: 'CandlestickEvent'):
+    candlestick_event.print_object()
+    print("\n")
+
+def error(e: 'HuobiApiException'):
+    print(e.error_code + e.error_message)
+
 
 def run():
-    actBalSer=AccountBalanceService()
-    balance=actBalSer.getSpotActUSDTBalance()
-    print("当前余额为："+balance)
+    # actBalSer=AccountBalanceService()
+    # balance=actBalSer.getSpotActUSDTBalance()
+    # print("当前余额为："+balance)
+
+
+    market_client = MarketClient()
+    market_client.sub_candlestick("btcusdt,ethusdt", CandlestickInterval.MIN5, callback, error)
 
