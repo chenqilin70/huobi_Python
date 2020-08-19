4029b8e5611c85ef3567f948bced1fa185dbe5a5
chenqilin70 <kylinchen85@foxmail.com>
2020-08-19 17:53:55 +0800

    测试git

--- alpha_lin/test.py
+++ alpha_lin/test.py
@@ -1,9 +1,11 @@
 
 from alpha_lin.service.AccountBalanceService import AccountBalanceService, MarketClient, CandlestickInterval
-
+import time
 
 def callback(candlestick_event: 'CandlestickEvent'):
-    print(type(candlestick_event),"\n")
+    print(type(candlestick_event.tick.id),"\n")
+    id=candlestick_event.tick.id
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(id)), candlestick_event.tick.close)
 
 def error(e: 'HuobiApiException'):
     print(e.error_code + e.error_message)
