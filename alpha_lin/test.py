6fc681877c42684789f2f965a69786566f250a29
chenqilin70 <kylinchen85@foxmail.com>
2020-08-19 12:50:39 +0800

    测试git

--- alpha_lin/test.py
+++ alpha_lin/test.py
@@ -6,10 +6,12 @@ def run():
     # Create generic client instance and get the timestamp
     generic_client = GenericClient()
     ts = generic_client.get_exchange_timestamp()
+    print(type(ts))
     print(ts)
+    print(ts["status"])
+    print(ts["data"])
 
     # Create the market client instance and get the latest btcusdt‘s candlestick
-    market_client = MarketClient()
-    list_obj = market_client.get_candlestick("btcusdt", CandlestickInterval.MIN5, 10)
-    LogInfo.output_list(list_obj)
-    print("d")
+    # market_client = MarketClient()
+    # list_obj = market_client.get_candlestick("btcusdt", CandlestickInterval.MIN5, 10)
+    # LogInfo.output_list(list_obj)
